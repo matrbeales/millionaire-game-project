b5d7f3083ee53e2eb3b4eb4f2b749063b8283b5b
unknown <mbeales@spartaglobal.com>
2018-12-16 13:27:19 +0000

    Added styles to both pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-
+var questions = [
+  {
+    question: "Which city hosted the Olympic games in 1992?",
+    answers: {
+      a: 'Paris',
+      b: 'Barcelona',
+      c: 'Berlin',
+      d: 'Rome'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "Who directed the 1990 film Miller's Crossing?",
+    answers: {
+      a: 'Steven Spielberg',
+      b: 'Quentin Tarantino',
+      c: 'The Coen Brothers',
+      d: 'James Cameron'
+    },
+    correctAnswer: 'c'
+  }
+]
